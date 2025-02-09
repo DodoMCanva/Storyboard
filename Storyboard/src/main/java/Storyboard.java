5d5c0a79edc9779acb2906b59883c0e8ad0eb74a
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-02-08 18:22:28 -0600

    Crear

---
+++ Storyboard/src/main/java/Storyboard.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Fer
+ */
+public class Storyboard {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
